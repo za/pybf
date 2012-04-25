c36c78cb2ae12594e4cd13cb51e91919b95dbb76
Zaki Akhmad <zakiakhmad@gmail.com>
2012-04-25 16:09:11 +0700

    enhance on how to declare a-z alphabet

--- pybf.py
+++ pybf.py
@@ -2,12 +2,20 @@
 
 import itertools
 import string
+import sys
 
-alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+def main():
+#    alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+    alphabet = string.lowercase[:26]
 
-n = raw_input("insert integer repeat number:")
-m = int(n)
+#    n = raw_input("insert the character size:")
+#    m = int(n)
+    o = sys.argv[1]
+    p = int(o)
 
-f = itertools.product(alphabet, repeat=m)
-for elem in f:
-    print elem
+    f = itertools.product(alphabet, repeat=p)
+    for elem in f:
+        print elem
+
+if __name__== '__main__':
+    main()
